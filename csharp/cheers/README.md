3dda08b2d605ee888744dc04047ef118cc592151
Chris Jarvis <clj@ollure.com>
2015-07-14 22:45:41 -0500

    changed text

---
+++ csharp/cheers/README.md
@@ -0,0 +1,21 @@
+## Cheers
+
+### Getting Started
+
+1. Fork this repo
+2. Clone your fork
+3. `cd` into the directory that represents your language of choice and start your project there.
+
+
+### What to Implement
+1. Getting user's name and print it.
+2. Cheer the user on.
+
+For example, if name is "Bob", output should be:
+
+```shell
+Give me a.. b
+Give me an.. o
+Give me a.. b
+BOB is.. GRAND!
+```
---
+++ csharp/cheers/cheers/cheer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace cheers
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			string name ="Bob"
+				
+			Console.WriteLine ("Go");
+
+		}
+	}
+}
+
+
+/*### What to Implement
+1. Getting user's name and print it.
+2. Cheer the user on.
+
+For example, if name is "Bob", output should be:
+
+```shell
+Give me a.. b
+Give me an.. o
+Give me a.. b
+BOB is.. GRAND!
+```
+adding text to test git
+*/
