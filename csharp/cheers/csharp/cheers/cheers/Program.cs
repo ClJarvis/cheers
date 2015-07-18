02ee92ec177991acb4065bff270df02d946d712b
Chris Jarvis <clj@ollure.com>
2015-07-18 01:34:41 -0500

    added readline

--- csharp/cheers/csharp/cheers/cheers/Program.cs
+++ csharp/cheers/csharp/cheers/cheers/Program.cs
@@ -5,8 +5,38 @@ namespace cheers
 	class MainClass
 	{
 		public static void Main (string[] args)
+
+	//	name[] delimitername = { ' ', ',', '.', ':', '\t' };
 		{
-		//	Console.WriteLine ("Hello World!");
+			Console.WriteLine ("What is your name?");
+			string response = Console.ReadLine();
+
+			//string name = "Chris";
+			Console.WriteLine (response + " is.. Grand");
+		//string[] words = name.Split(name);
+			System.Console.WriteLine("{0} letters in", + response.Length);
+
+		//	foreach (string s in name)
+			{
+			//	System.Console.WriteLine("Give me a.. "+ s);
+			}
 		}
 	}
 }
+
+
+
+
+/*### What to Implement
+1. Getting user's name and print it.
+2. Cheer the user on.
+
+For example, if name is "Bob", output should be:
+
+```shell
+Give me a.. b
+Give me an.. o
+Give me a.. b
+BOB is.. GRAND!
+```
+*/
---
+++ csharp/cheers/csharp/cheers/cheers/cheers2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Cheers
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("What is your name?");
+			var name = Console.ReadLine();
+			foreach (char l in name)
+			{
+				if ("aefhilmnosxAEFHILMNOSX".Contains(l))
+				{
+					Console.WriteLine("Give me an ...  {0}", l);
+				}
+				else Console.WriteLine("Give me a ..... {0}", l);
+			}
+			Console.WriteLine("{0} is ... GRAND", name);
+			Console.ReadLine();
+
+		}
+
+	}
+}
